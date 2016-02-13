eb111a3ff3075e080f93bc090718c379c701a012
Philipp J Roesch <philipp.stats@gmail.com>
2016-02-13 22:10:57 +0100

    add convertCharacterToFactor

---
+++ R/convertCharacterToFactor.R
@@ -0,0 +1,19 @@
+#' Convert character features to factor features in data.frames
+#' 
+#' @param data [\code{data.frame}]
+#' @import checkmate
+#' 
+#' @export
+#' @example 
+#' data("mpg", package = "ggplot2")
+#' str(mpg)
+#' convertCharacterToFactor(mpg)
+#' str(mpg)
+
+convertCharacterToFactor = function(data) {
+  assertClass(data, "data.frame")
+  
+  idx = sapply(data, is.character)
+  data[i] = lapply(data[idx], as.factor)
+  data
+}

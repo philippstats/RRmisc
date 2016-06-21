ae8f600a004a0dcf30246b19d43400057c24b734
philippstats <philipp.stats@gmail.com>
2016-06-21 08:43:10 +0200

    add data and tasks-fct

---
+++ R/MTcreateTasksList.R
@@ -0,0 +1,9 @@
+#' MT Create Tasks List
+#' @export
+
+MTcreateTasksList = function() {
+  list(adult = adult, calhousing = calhousing, BNGbreastw = BNGbreastw, 
+    Clickpredictionsmall = Clickpredictionsmall, electricity = electricity, 
+    twodplanes = twodplanes, fried = fried, letter1 = letter1, letter2 = letter2)
+}
+  
---
+++ data-raw/getD2.R
@@ -0,0 +1,36 @@
+adult = tasks[[1]]
+adult$task.desc$id = "adult"
+save(adult, file = "adult.RData")
+
+calhousing = tasks[[2]]
+calhousing$task.desc$id = "calhousing"
+save(adult, file = "adult.RData")
+
+BNGbreastw = tasks[[3]]
+BNGbreastw$task.desc$id = "BNGbreastw"
+save(adult, file = "adult.RData")
+
+Clickpredictionsmall = tasks[[4]]
+Clickpredictionsmall$task.desc$id = "Clickpredictionsmall"
+save(Clickpredictionsmall, file = "Clickpredictionsmall.RData")
+
+electricity = tasks[[5]]
+electricity$task.desc$id = "electricity"
+save(electricity, file = "electricity.RData")
+
+twodplanes = tasks[[6]]
+twodplanes$task.desc$id = "twodplanes"
+save(twodplanes, file = "twodplanes.RData")
+
+fried = tasks[[7]]
+fried$task.desc$id = "fried"
+save(fried, file = "fried.RData")
+
+letter1 = tasks[[8]]
+letter1$task.desc$id = "letter1"
+save(letter1, file = "letter1.RData")
+
+letter2 = tasks[[9]]
+letter2$task.desc$id = "letter2"
+save(letter2, file = "letter2.RData")
+
